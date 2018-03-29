bd87e6b88158d2e5f6bc39fa3ee23063a3ae104b
ltsuda <leonardo.tsuda@gmail.com>
2018-03-29 14:55:35 -0300

    Add NodeJS - Aulas 1-7

---
+++ alura/dev-node/casadocodigo/app/routes/produtos.js
@@ -0,0 +1,53 @@
+module.exports = function(app) {
+  var listarProdutos = function(req, res) {
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+    produtosDAO.lista(function(err, results) {
+      res.format({
+        html: function() {
+          res.render('produtos/lista', { lista: results });
+        },
+        json: function() {
+          res.json(results);
+        }
+      });
+    });
+    connection.end();
+  };
+
+  app.get('/produtos', listarProdutos);
+  app.get('/produtos/form', function(req, res) {
+    res.render('produtos/form', { errosValidacao: {}, produto: {} });
+  });
+
+  app.post('/produtos', function(req, res) {
+    var produto = req.body;
+    console.log(produto);
+
+    req.assert('titulo', 'Titulo é obrigatorio').notEmpty();
+    req.assert('preco', 'Formato inválido').isFloat();
+
+    var erros = req.validationErrors();
+
+    if(erros) {
+      res.format({
+        html: function() {
+          res.status(400).render('produtos/form', { errosValidacao: erros, produto: produto });
+        },
+        json: function() {
+          res.status(400).json(erros);
+        }
+      });
+      return;
+    }
+
+
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.salva(produto, function(err, results) {
+      res.redirect('/produtos');
+    });
+    connection.end();
+  });
+};
