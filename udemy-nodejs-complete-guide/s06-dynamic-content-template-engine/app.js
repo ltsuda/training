a55344e0b6d0045d7a87d2e193aab32df01c1d55
ltsuda <leonardo.tsuda@icloud.com>
2019-04-02 13:19:07 -0300

    add s06e77 shared data across request and users

---
+++ udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const adminData = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
+const rootDir = require('./util/path.js')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+})
+
+app.listen(3000)
