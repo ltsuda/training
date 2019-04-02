28c3523bd15e99229c48a021c5e4af15b2a97120
ltsuda <leonardo.tsuda@icloud.com>
2019-04-02 15:42:13 -0300

    add s06e84 working with pug

--- udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
+++ udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
@@ -8,6 +8,9 @@ const rootDir = require('./util/path.js')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: '404'})
 })
 
 app.listen(3000)
