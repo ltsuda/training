25b97a4e99e700244075233e03fbd8eb3da9c4b4
ltsuda <leonardo.tsuda@icloud.com>
2019-04-02 16:22:27 -0300

    add s06e87 working with handlebars

--- udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
+++ udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const handleBars = require('express-handlebars')
 
 const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
@@ -8,7 +9,8 @@ const rootDir = require('./util/path.js')
 
 const app = express()
 
-app.set('view engine', 'pug')
+app.engine('handlebars', handleBars({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }))
+app.set('view engine', 'handlebars')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,7 +20,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found!'})
 })
 
 app.listen(3000)
