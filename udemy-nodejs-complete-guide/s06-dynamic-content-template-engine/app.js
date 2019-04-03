316aad6ff3d51696d86aa61bb0bd44faca671ba9
ltsuda <leonardo.tsuda@icloud.com>
2019-04-03 10:21:05 -0300

    add s06e91 working with ejs
    Also, removes pug and handlebars as it won't be used anymore.

--- udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
+++ udemy-nodejs-complete-guide/s06-dynamic-content-template-engine/app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const handleBars = require('express-handlebars')
 
 const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
@@ -9,8 +8,7 @@ const rootDir = require('./util/path.js')
 
 const app = express()
 
-app.engine('handlebars', handleBars({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout' }))
-app.set('view engine', 'handlebars')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,7 +18,7 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found!'})
+  res.status(404).render('404', { pageTitle: 'Page Not Found!' })
 })
 
 app.listen(3000)
